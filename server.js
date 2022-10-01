1cb3f7f0b5973439aa9dfb1ff61ec9dee65e0b7a
drewrvaz <drewrvaz@gmail.com>
2022-10-01 17:40:23 -0400

    Changed the way I'm calling the route sin server.js

--- server.js
+++ server.js
@@ -3,28 +3,35 @@ const path = require("path");
 
 // Creates the port for the environment
 const PORT = process.env.PORT || 3001;
+
+// App uses express
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true}));
-
 app.use(express.json());
+
+// Creates a route for every file in the public folder
 app.use(express.static("public"));
 
+// Routes to the route files
+app.require('./routes/api');
+app.require('./routes.html');
+
 // GET route for notes page
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/notes.html'))
+// })
 
 // GET route for homepage
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// });
 
 // GET route for saved notes
-app.get('/api/notes', (req, res) => {
-  res.send
-})
+// app.get('/api/notes', (req, res) => {
+//   res.send
+// })
 
 // app listener - starts the server
 app.listen(PORT, () =>
