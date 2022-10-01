d8b5aa5e6e85763419f1749ef7adba31f74bf294
drewrvaz <drewrvaz@gmail.com>
2022-10-01 17:18:19 -0400

    Pushing to Heroku

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const path = require("path");
 
-// Consts for routes
-const apiRoutes = require("./routes/api");
-const htmlRoutes = require("./routes/html");
-
 // Creates the port for the environment
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,6 +10,21 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(express.json());
 app.use(express.static("public"));
+
+// GET route for notes page
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+// GET route for homepage
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+// GET route for saved notes
+app.get('/api/notes', (req, res) => {
+  res.send
+})
 
 // app listener - starts the server
 app.listen(PORT, () =>
