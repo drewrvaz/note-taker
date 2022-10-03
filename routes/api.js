29b9909045f2e9a109f4109a00ee466fb07024e9
drewrvaz <drewrvaz@gmail.com>
2022-10-03 14:38:12 -0400

    Tried some things, still can't get the notes to post. Will probably turn in as is and try and refactor later

--- routes/api.js
+++ routes/api.js
@@ -1,56 +1,17 @@
 // Required dependencies
-// const notes = require('express').Router();
-// const {v4 : uuidv4} = require("uuid")
-// const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
-
-// notes.get('/', (req, res) =>
-//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-// );
-
-// // POST Route for submitting feedback
-// notes.post('/', (req, res) => {
-//   // Destructuring assignment for the items in req.body
-//   const { title, text } = req.body;
-
-//   // If all the required properties are present
-//   if (title && text) {
-//     // Variable for the object we will save
-//     const newNote = {
-//       title,
-//       text,
-//       note_id: uuidv4(),
-//     };
-
-//     readAndAppend(newNote, './db/db.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newNote,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting note');
-//   }
-// });
-
-
 const notes = require('express').Router();
 // npm package for creation of unique IDs
 const {v4 : uuidv4} = require("uuid")
 const fs = require ('fs');
 
-// npm package for creation of unique IDs
-
-
 // GET /api/notes should read db.json file and then return all of the saved notes as JSON
-notes.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../db/db.json'));
+notes.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 // POST /api/notes should get a new note to save on the request body
 // It should add the new note to the db.json file, and then return the new note to the client
-notes.post('/', (req, res) => {
+notes.post('/notes', (req, res) => {
   let db = fs.readFileSync('db/db.json');
   db = JSON.parse(db);
   res.json(db);
