51368487676e4a2b5b1d0886045e7f5ed1cc33ad
drewrvaz <drewrvaz@gmail.com>
2022-10-02 21:30:50 -0400

    Removed index.js, reqrote api.js route, also rewrote some of server.js in an attempt to troubleshoot. We'll see if it worked

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,71 @@
 // Required dependencies
-const express = require('express');
-const router = express.Router();
-const path = require ('path');
-const fs = require ('fs');
-
-// npm package for creation of unique IDs
+const notes = require('express').Router();
 const {v4 : uuidv4} = require("uuid")
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
-// GET /api/notes should read db.json file and then return all of the saved notes as JSON
-router.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../db/db.json'));
+fb.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+fb.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, note } = req.body;
+
+  // If all the required properties are present
+  if (title && note) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      note,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
 });
 
-// POST /api/notes should get a new note to save on the request body
-// It should add the new note to the db.json file, and then return the new note to the client
-router.post('/api/notes', (req, res) => {
-  let db = fs.readFileSync('db/db.json');
-  db = JSON.parse(db);
-  res.json(db);
-  // Creates the body of the notes
-  let userNote = {
-    title: req.body.title,
-    text: req.body.text,
-    // Creates unique ID for each of the created notes
-    id: uuidv4()
-  };
-  db.push(userNote);
-  fs.writeFileSync('db/db.json', JSON.stringify(db));
-  res.json(db);
-});
+module.exports = notes;
 
-module.exports = router;
+
+// const express = require('express');
+// const router = express.Router();
+// const path = require ('path');
+// const fs = require ('fs');
+
+// // npm package for creation of unique IDs
+// 
+
+// // GET /api/notes should read db.json file and then return all of the saved notes as JSON
+// router.get('/api/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../db/db.json'));
+// });
+
+// // POST /api/notes should get a new note to save on the request body
+// // It should add the new note to the db.json file, and then return the new note to the client
+// router.post('/api/notes', (req, res) => {
+//   let db = fs.readFileSync('db/db.json');
+//   db = JSON.parse(db);
+//   res.json(db);
+//   // Creates the body of the notes
+//   let userNote = {
+//     title: req.body.title,
+//     text: req.body.text,
+//     // Creates unique ID for each of the created notes
+//     id: uuidv4()
+//   };
+//   db.push(userNote);
+//   fs.writeFileSync('db/db.json', JSON.stringify(db));
+//   res.json(db);
+// });
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,59 +1,34 @@
-const express = require("express");
-const path = require("path"); 
+// Required dependencies
+const express = require('express');
+const path = require('path'); 
+const api = require('./routes/api')
 
 // Creates the port for the environment
 const PORT = process.env.PORT || 3001;
 
 // App uses express
 const app = express();
-const fs = require ('fs');
 
 // npm package for creation of unique IDs
-const {v4 : uuidv4} = require("uuid")
+const {v4 : uuidv4} = require('uuid')
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use('/api', api);
 
 // Creates a route for every file in the public folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-});
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
-// GET route for notes
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'))
-});
-
-// GET /api/notes should read db.json file and then return all of the saved notes as JSON
-router.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../db/db.json'));
-});
-
-// POST /api/notes should get a new note to save on the request body
-// It should add the new note to the db.json file, and then return the new note to the client
-router.post('/api/notes', (req, res) => {
-  let db = fs.readFileSync('db/db.json');
-  db = JSON.parse(db);
-  res.json(db);
-  // Creates the body of the notes
-  let userNote = {
-    title: req.body.title,
-    text: req.body.text,
-    // Creates unique ID for each of the created notes
-    id: uuidv4()
-  };
-  db.push(userNote);
-  fs.writeFileSync('db/db.json', JSON.stringify(db));
-  res.json(db);
-});
-
-// Routes
-// app.use('/api', require('./routes/api'));
-// app.use('/html', require('./routes/html'));
+// GET route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 // app listener - starts the server
 app.listen(PORT, () =>
