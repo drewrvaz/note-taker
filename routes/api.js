74e57059557e0b73832335755bd1711e6bcf6089
drewrvaz <drewrvaz@gmail.com>
2022-10-03 13:23:19 -0400

    Trying to trouble shoot why I can't post a new note

--- routes/api.js
+++ routes/api.js
@@ -1,71 +1,69 @@
 // Required dependencies
+// const notes = require('express').Router();
+// const {v4 : uuidv4} = require("uuid")
+// const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// notes.get('/', (req, res) =>
+//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+// );
+
+// // POST Route for submitting feedback
+// notes.post('/', (req, res) => {
+//   // Destructuring assignment for the items in req.body
+//   const { title, text } = req.body;
+
+//   // If all the required properties are present
+//   if (title && text) {
+//     // Variable for the object we will save
+//     const newNote = {
+//       title,
+//       text,
+//       note_id: uuidv4(),
+//     };
+
+//     readAndAppend(newNote, './db/db.json');
+
+//     const response = {
+//       status: 'success',
+//       body: newNote,
+//     };
+
+//     res.json(response);
+//   } else {
+//     res.json('Error in posting note');
+//   }
+// });
+
+
 const notes = require('express').Router();
+// npm package for creation of unique IDs
 const {v4 : uuidv4} = require("uuid")
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const fs = require ('fs');
 
-notes.get('/', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-);
+// npm package for creation of unique IDs
 
-// POST Route for submitting feedback
+
+// GET /api/notes should read db.json file and then return all of the saved notes as JSON
+notes.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+// POST /api/notes should get a new note to save on the request body
+// It should add the new note to the db.json file, and then return the new note to the client
 notes.post('/', (req, res) => {
-  // Destructuring assignment for the items in req.body
-  const { title, note } = req.body;
-
-  // If all the required properties are present
-  if (title && note) {
-    // Variable for the object we will save
-    const newNote = {
-      title,
-      note,
-      note_id: uuidv4(),
-    };
-
-    readAndAppend(newNote, './db/db.json');
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    res.json(response);
-  } else {
-    res.json('Error in posting feedback');
-  }
+  let db = fs.readFileSync('db/db.json');
+  db = JSON.parse(db);
+  res.json(db);
+  // Creates the body of the notes
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    // Creates unique ID for each of the created notes
+    id: uuidv4()
+  };
+  db.push(newNote);
+  fs.writeFileSync('db/db.json', JSON.stringify(db));
+  res.json(db);
 });
 
 module.exports = notes;
-
-
-// const express = require('express');
-// const router = express.Router();
-// const path = require ('path');
-// const fs = require ('fs');
-
-// // npm package for creation of unique IDs
-// 
-
-// // GET /api/notes should read db.json file and then return all of the saved notes as JSON
-// router.get('/api/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../db/db.json'));
-// });
-
-// // POST /api/notes should get a new note to save on the request body
-// // It should add the new note to the db.json file, and then return the new note to the client
-// router.post('/api/notes', (req, res) => {
-//   let db = fs.readFileSync('db/db.json');
-//   db = JSON.parse(db);
-//   res.json(db);
-//   // Creates the body of the notes
-//   let userNote = {
-//     title: req.body.title,
-//     text: req.body.text,
-//     // Creates unique ID for each of the created notes
-//     id: uuidv4()
-//   };
-//   db.push(userNote);
-//   fs.writeFileSync('db/db.json', JSON.stringify(db));
-//   res.json(db);
-// });
-
-// module.exports = router;
